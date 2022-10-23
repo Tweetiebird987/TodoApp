9b9d19032cdb67405aae33427121fb102a83e5a5
Tweetiebird987 <sarelerasmus856@gmail.com>
2022-10-23 16:57:14 -0700

    Start of Todo App Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Arnold's Todo List</h1>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
