0d67623f6be1516c6cb7b90353bc73e8984646a0
Tweetiebird987 <sarelerasmus856@gmail.com>
2022-10-23 20:24:56 -0700

    Create Input Portion of Todo List

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import './App.css';
 
+//Import components
+import Form from './components/Form';
+
 function App() {
+  
   return (
     <div className="App">
       <header>
         <h1>Arnold's Todo List</h1>
       </header>
-
+      <Form />
     </div>
   );
 }
